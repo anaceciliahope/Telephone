2be6806e016792a3cedc6234935e11bd51420352
anaceciliahope <anaceciliahope@gmail.com>
2024-04-11 18:51:08 -0300

    Criando contato repository

--- src/main/java/br/com/telefone/config/listener/TelefoneListener.java
+++ src/main/java/br/com/telefone/config/listener/TelefoneListener.java
@@ -1,7 +1,9 @@
 package br.com.telefone.config.listener;
 
+import br.com.telefone.model.Contato;
 import br.com.telefone.model.Telefone;
 import br.com.telefone.model.enums.TipoTelefone;
+import br.com.telefone.repository.ContatoRepository;
 import br.com.telefone.repository.TelefoneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -14,6 +16,9 @@ public class TelefoneListener {
     @Autowired
     private TelefoneRepository telefoneRepository;
 
+    @Autowired
+    private ContatoRepository contatoRepository;
+
     @EventListener
     public void handleContextRefresh(ContextRefreshedEvent event) {
         System.out.println("O sistema está em execução. ");
@@ -22,6 +27,10 @@ public class TelefoneListener {
         telefone.setTipoTelefone(TipoTelefone.TELEFONE_PRINCIPAl);
         telefone = telefoneRepository.save(telefone);
         System.out.println(telefone);
+
+        Contato contato = new Contato("Ana");
+        contato = contatoRepository.save(contato);
+        System.out.println(contato);
 
 
     }
